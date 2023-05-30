10d8977c08be164e92e03d07167c15dfb6ea5850
Akio1102 <cristiandaviddiazaza@gmail.com>
2023-05-30 13:29:05 -0500

    feat (SUPERMARKET) : 🚧 Class Productos
    Creación de la clase de Productos para hacer el crud

--- SUPERMARKET/Models/Productos.php
+++ SUPERMARKET/Models/Productos.php
@@ -4,60 +4,107 @@ require_once("Pdo.php");
 
 class Productos extends ConexiónPdo{
     
+    private $productoId;
+    private $categoriasId;
+    private $precioUnitario;
+    private $stock;
+    private $unidadesPedidas;
     private $proveedorId;
-    private $nombre;
-    private $telefono;
-    private $ciudad;
+    private $nombreProducto;
+    private $descontinuado;
 
-    public function __construct($proveedorId= 0, $nombre= "", $telefono=0, $ciudad=""){
+    public function __construct($productoId= 0, $categoriasId= 0, $precioUnitario=0, $stock=0,$unidadesPedidas=0,$proveedorId=0,$nombreProducto="",$descontinuado=0){
+        $this->productoId = $productoId;
+        $this->categoriasId = $categoriasId;
+        $this->precioUnitario = $precioUnitario;
+        $this->stock = $stock;
+        $this->unidadesPedidas = $unidadesPedidas;
         $this->proveedorId = $proveedorId;
-        $this->nombre = $nombre;
-        $this->telefono = $telefono;
-        $this->ciudad = $ciudad;
+        $this->nombreProducto = $nombreProducto;
+        $this->descontinuado = $descontinuado;
         parent::__construct();
     }
     
     //Getters
+    public function getProductoId(){
+        return $this->productoId;
+    }
+
+    public function getCategoriasId(){
+        return $this->categoriasId;
+    }
+
+    public function getPrecioUnitario(){
+        return $this->precioUnitario;
+    }
+
+    public function getStock(){
+        return $this->stock;
+    }
+
+    public function getUnidadesPedidas(){
+        return $this->unidadesPedidas;
+    }
+
     public function getProveedorId(){
         return $this->proveedorId;
     }
 
-    public function getNombre(){
-        return $this->nombre;
+    public function getNombreProducto(){
+        return $this->nombreProducto;
     }
 
-    public function getTelefono(){
-        return $this->telefono;
+    public function getDescontinuado(){
+        return $this->descontinuado;
     }
 
-    public function getCiudad(){
-        return $this->ciudad;
-    }
 
     //Setters
+    public function setProductoId($productoId){
+        $this->productoId =$productoId;
+    }
+
+    public function setCategoriasId($categoriasId){
+        $this->categoriasId =$categoriasId;
+    }
+
+    public function setPrecioUnitario($precioUnitario){
+        $this->precioUnitario =$precioUnitario;
+    }
+
+    public function setStock($stock){
+        $this->stock =$stock;
+    }
+
+    public function setUnidadesPedidas($unidadesPedidas){
+        $this->unidadesPedidas =$unidadesPedidas;
+    }
+
     public function setProveedorId($proveedorId){
         $this->proveedorId =$proveedorId;
     }
 
-    public function setNombre($nombre){
-        $this->nombre =$nombre;
+    public function setNombreProducto($nombreProducto){
+        $this->nombreProducto =$nombreProducto;
     }
 
-    public function setTelefono($telefono){
-        $this->telefono =$telefono;
+    public function setDescontinuado($descontinuado){
+        $this->descontinuado =$descontinuado;
     }
 
-    public function setCiudad($ciudad){
-        $this->ciudad =$ciudad;
-    }
+
 
     public function insertData(){
         try {
-            $stm = $this-> dbCnx -> prepare("INSERT INTO productos(nombre,telefono,ciudad) 
-            VALUES (:nomb,:cel,:dire)");
-            $stm->bindParam(":nomb",$this->nombre);
-            $stm->bindParam(":cel",$this->telefono);
-            $stm->bindParam(":dire",$this->ciudad);
+            $stm = $this-> dbCnx -> prepare("INSERT INTO productos(categoriasId,precioUnitario,stock,unidadesPedidas,proveedorId,nombreProducto,descontinuado) 
+            VALUES (:cateID,:precio,:stock,:unidades,:proveID,:namePro,:descontinuado)");
+            $stm->bindParam(":cateID",$this->categoriasId);
+            $stm->bindParam(":precio",$this->precioUnitario);
+            $stm->bindParam(":stock",$this->stock);
+            $stm->bindParam(":unidades",$this->unidadesPedidas);
+            $stm->bindParam(":proveID",$this->proveedorId);
+            $stm->bindParam(":namePro",$this->nombreProducto);
+            $stm->bindParam(":descontinuado",$this->descontinuado);
             $stm->execute();
         } catch (Exception $e) {
             return $e->getMessages();
@@ -76,8 +123,8 @@ class Productos extends ConexiónPdo{
     
     public function delete(){
         try {
-            $stm = $this-> dbCnx -> prepare("DELETE FROM productos WHERE proveedorId = :id");
-            $stm->bindParam(":id",$this->proveedorId);
+            $stm = $this-> dbCnx -> prepare("DELETE FROM productos WHERE productoId = :id");
+            $stm->bindParam(":id",$this->productoId);
             $stm -> execute();
             return $stm -> fetchAll();
         } catch (Exception $e) {
@@ -87,8 +134,8 @@ class Productos extends ConexiónPdo{
     
     public function selectOne(){
         try {
-            $stm = $this-> dbCnx -> prepare("SELECT * FROM productos WHERE proveedorId = :id");
-            $stm->bindParam(":id",$this->proveedorId);
+            $stm = $this-> dbCnx -> prepare("SELECT * FROM productos WHERE productoId = :id");
+            $stm->bindParam(":id",$this->productoId);
             $stm -> execute();
             return $stm -> fetchAll();
         } catch (Exception $e) {
@@ -98,12 +145,12 @@ class Productos extends ConexiónPdo{
 
     public function update(){
         try {
-            $stm = $this-> dbCnx -> prepare("UPDATE productos SET nombre=:nomb , telefono=:tel , ciudad=:city
-            WHERE proveedorId = :id");
-            $stm->bindParam(":id",$this->proveedorId);
-            $stm->bindParam(":nomb",$this->nombre);
-            $stm->bindParam(":tel",$this->telefono);
-            $stm->bindParam(":city",$this->ciudad);
+            $stm = $this-> dbCnx -> prepare("UPDATE productos SET categoriasId=:nomb , precioUnitario=:tel , stock=:city , unidadesPedidas=, proveedorId=, nombreProducto,descontinuado
+            WHERE productoId = :id");
+            $stm->bindParam(":id",$this->productoId);
+            $stm->bindParam(":nomb",$this->categoriasId);
+            $stm->bindParam(":tel",$this->precioUnitario);
+            $stm->bindParam(":city",$this->stock);
             $stm -> execute();
             return $stm -> fetchAll();
         } catch (Exception $e) {
@@ -112,3 +159,6 @@ class Productos extends ConexiónPdo{
     }
 }
 ?>
+
+ productos(categoriasId,precioUnitario,stock,unidadesPedidas,proveedorId,nombreProducto,descontinuado) 
+            VALUES (:cateID,:precio,:stock,:unidades,:proveID,:namePro,:descontinuado)
